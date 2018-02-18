0ed096871a72cf465aa005bcb53fb17ff8691d77
jacob1692 <jacob.hernandezsanchez@epfl.ch>
2018-02-18 19:32:06 +0100

    separate in two pakages

---
+++ foot_control_xy/README.md
@@ -0,0 +1,3 @@
+# foot_control_xy
+
+1. run /dev/init.sh to set up the model path in gazebo.
---
+++ foot_control_xy/dev/init_setup.sh
@@ -0,0 +1,2 @@
+echo "source $(pwd)/gz_model_path_update.sh" >> ~/.bashrc
+echo "info: set up in bashrc done!"
