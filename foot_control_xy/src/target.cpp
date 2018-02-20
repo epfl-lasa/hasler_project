7ba2abf88f076c8fe2e8e5cc159eaf902ab74388
jacob1692 <jacob.hernandezsanchez@epfl.ch>
2018-02-20 10:31:24 +0100

    last commit 20-feb

---
+++ foot_control_xy/src/target.cpp
@@ -0,0 +1,28 @@
+#include "foot_control_xy/target.h"
+#include <geometry_msgs/TransformStamped.h>
+#include <tf2_kdl/tf2_kdl.h>
+
+using namespace std;
+using namespace ros;
+
+namespace foot_control_xy 
+{
+	Target::Target (const ros::NodeHandle& nh): nh_(nh){
+		 //!velocity_sub_=nh_.subscribe("cmd_vel", 1, &Turtle::velocityCallback, this); in case we want to add moving objects
+		 pose_pub_=nh_.advertise<PoseStamped>{"pose_stamped",1};
+		 teleport_absolute_srv_ = nh_.advertiseService("teleport_absolute",&Target::teleportAbsoluteCallback, this);
+	}
+	
+bool Target::teleportAbsoluteCallback(foot_control_xy::TeleportAbsolute::Request& req, foot_control_xy::TeleportAbsolute::Response&)
+{
+  teleport_requests_.push_back(TeleportRequest(req.x, req.y, req.theta, 0, false));
+  return true;
+}	
+
+
+bool Target::update (double dt)
+{
+	bool modified=false;
+
+}
+	
