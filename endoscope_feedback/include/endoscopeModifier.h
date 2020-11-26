d87bb543fcffde6edd01bbc57ec70e84fe26dfa2
jacob <jacob.hernandezsanchez@epfl.ch>
2020-11-26 16:53:03 +0100

    adapt the visual feedack to different resolutions

--- endoscope_feedback/include/endoscopeModifier.h
+++ endoscope_feedback/include/endoscopeModifier.h
@@ -2,6 +2,7 @@
 #define __endoscopeModifier_H__
 
 #include <ros/ros.h>
+#include <ros/package.h>
 #include <image_transport/image_transport.h>
 #include <cv_bridge/cv_bridge.h>
 #include <sensor_msgs/image_encodings.h>
@@ -12,12 +13,13 @@
 #include <signal.h>
 #include <mutex>
 #include <string>
-#include <fstream>
+#include <fstream>  
 #include <vector>
 #include <utility> // std::pair
 #include <stdexcept> // std::runtime_error
 #include <sstream> // std::stringstream
 #include "Utils_math.h"
+#include "endoscope_feedback/SurgicalTaskStateMsg.h"
 
 static const std::string OPENCV_WINDOW = "Image window";
 
@@ -25,11 +27,12 @@ static const std::string OPENCV_WINDOW = "Image window";
 
 using namespace std;
 using namespace Eigen;
+using namespace cv;
 
 class endoscopeModifier {
 
 public:
-
+  enum tool_Types{ENDOSCOPE_T, GRASPER_T, NB_TOOLS_TYPES};
   enum tool_States {INSERTION_STATE, INSIDE_TROCAR_STATE};
   enum toolH_States {HUMAN_IDLE, HUMAN_ENGAGED, HUMAN_CONFUSED}; 
   enum tool_Axis {Y_TOOL_AXIS,X_TOOL_AXIS,Z_TOOL_AXIS,ROLL_TOOL_AXIS,GRASPING_TOOL_AXIS, NB_AXIS_TOOL};
@@ -43,17 +46,15 @@ private:
   std::mutex _mutex;
   static endoscopeModifier *me;
   
-  tool_States _endoscopeState;   
-  tool_States _grasperState;   
+  tool_States _toolState[NB_TOOLS_TYPES];     
   
-  toolH_States _endoscopeHState;   
-  toolH_States _grasperHState;   
+  toolH_States _toolHState[NB_TOOLS_TYPES];   
 
   // ros::NodeHandle _nh;
   image_transport::ImageTransport* _it;
   image_transport::Subscriber _myImageSub;
   image_transport::Publisher _myOverlayedImagePub;
-
+  ros::Subscriber _surgicalTaskStateSub;
     
   // METHODS
 public:
@@ -68,19 +69,36 @@ private:
   cv_bridge::CvImagePtr _myCVPtr; // Original
   cv_bridge::CvImagePtr _myCVPtrCopy; // Original
 
+  //cv
+    cv::Mat _warning_icon_resized;
+
   //Eigen
-    Matrix<bool,NB_AXIS_TOOL,1> _endoscopeDOFEnable;
-    Matrix<bool,NB_AXIS_TOOL,1> _grasperDOFEnable;
+  
+    Array2f _feedIMGDims;
+    Array2i _toolTextCoord[NB_TOOLS_TYPES];
+
+    
+
+    Matrix<bool,NB_AXIS_TOOL,1> _toolDOFEnable[NB_TOOLS_TYPES];
+    
   //Flag
 
+    bool _flagImageReceivedOnce;
     volatile bool _flagImageReceived;
-
+    volatile bool _flagSurgicalTaskStateReceived;
+    void loadIcons();
     void imageCb(const sensor_msgs::ImageConstPtr& msg);
     void changeAndPublishImage();
+    void readSurgicalTaskState(const endoscope_feedback::SurgicalTaskStateMsgConstPtr& msg);
+    void drawTransparency(cv::Mat frame, cv::Mat transp, int xPos, int yPos);
+
+    void putTextForTool(uint toolN_ ,tool_States toolState_, cv::Point point_, cv::Scalar color_);
     static void stopNode(int sig);
 
 
-  
-
+    int _iconHeight;  
+  // Utils
+    cv::Point2i eigenV2iToCv(Eigen::Vector2i eigenVec2i_);
+    
 };
 #endif // __endoscopeModifier_H__
--- endoscope_feedback/src/endoscopeModifier.cpp
+++ endoscope_feedback/src/endoscopeModifier.cpp
@@ -1,30 +1,52 @@
   #include "endoscopeModifier.h"
+
+
+
+ const Eigen::Array2f R_TOOL_TXT_FB[] = { Eigen::Array2f(20.0f/480.0f,30.0f/640.0f), 
+                                          Eigen::Array2f(410.0f/480.0f,30.0f/640.0f)};
+
+ const float R_WARNING_ICON = 50.0f/480.0f;
   
+ const cv::Scalar COLOR_TOOL_TXT[] = {cv::Scalar(255,128,0), 
+                                      cv::Scalar(0,128,255)};
+ 
 endoscopeModifier *endoscopeModifier::me = NULL;
 
 char const *toolState_Names[]{"insertion mode", "moving inside"};
+char const *toolID_Names[]{"e: ", "g: "};
 
 endoscopeModifier::endoscopeModifier(ros::NodeHandle &nh, float frequency)
     : _nh(nh), _loopRate(frequency), _dt(1.0f / frequency){
     me = this;
     _it = new image_transport::ImageTransport(_nh);
-    _stop = false;    
-    _endoscopeDOFEnable.setZero();
-    _grasperDOFEnable.setZero();
+    _stop = false; 
+    for (size_t i = 0; i < NB_TOOLS_TYPES; i++)
+    {
+      _toolDOFEnable[i].setZero();  
+      _toolTextCoord[i].setZero();
+      _toolState[i]=INSERTION_STATE;
+    }
+       
+    _feedIMGDims.setZero();
+    _flagImageReceivedOnce=false;
     _flagImageReceived=false;
-    _endoscopeState=INSERTION_STATE;
-    _grasperState=INSIDE_TROCAR_STATE;
+    _flagSurgicalTaskStateReceived = true;
+    _iconHeight=50;
+  
 }
 
 endoscopeModifier::~endoscopeModifier()
 {
   cv::destroyWindow(OPENCV_WINDOW);
-  // delete(_it);
+  delete(_it);
 }
 
 
 bool endoscopeModifier::init() 
 {
+  _surgicalTaskStateSub = _nh.subscribe<endoscope_feedback::SurgicalTaskStateMsg>( "/surgicalTaskState"
+    , 1, boost::bind(&endoscopeModifier::readSurgicalTaskState, this, _1),
+    ros::VoidPtr(), ros::TransportHints().reliable().tcpNoDelay());
   _myImageSub = _it->subscribe("/cv_camera/image_raw", 1,
       &endoscopeModifier::imageCb, this);
   _myOverlayedImagePub = _it->advertise("/image_converter/output_video", 1);
@@ -79,6 +101,24 @@ void endoscopeModifier::imageCb(const sensor_msgs::ImageConstPtr& msg)
       return;
     }
 
+    if (!_flagImageReceivedOnce)
+    {
+      if (_myCVPtr->image.rows > 60 && _myCVPtr->image.cols > 60)
+      {
+         _feedIMGDims<<_myCVPtr->image.rows, _myCVPtr->image.cols;
+         for (size_t i = 0; i < NB_TOOLS_TYPES; i++)
+         {
+            _toolTextCoord[i] = (_feedIMGDims * R_TOOL_TXT_FB[i]).cast<int>();
+         }
+        loadIcons();
+      }
+      else
+      {
+        ROS_ERROR("The image received is corrupted (less than 60x60 pixels)");
+      }
+      _flagImageReceivedOnce=true;
+    }
+
     _flagImageReceived=true;
 
 }
@@ -87,42 +127,71 @@ void endoscopeModifier::changeAndPublishImage()
 {
   // Information on the video stream
     if (_myCVPtr->image.rows > 60 && _myCVPtr->image.cols > 60)
-
-    _myCVPtrCopy = _myCVPtr;
-
     cv::Size sourceImgSize = _myCVPtr->image.size();
+    _myCVPtrCopy = _myCVPtr;
+    for (size_t i = 0; i < NB_TOOLS_TYPES; i++)
+    {
+      putTextForTool(i, _toolState[i],eigenV2iToCv(_toolTextCoord[i]),COLOR_TOOL_TXT[i]);
+      if(_toolState[i]==INSERTION_STATE)
+      {
+        drawTransparency(_myCVPtrCopy->image,_warning_icon_resized,_toolTextCoord[i].x() + 100,_toolTextCoord[i].y());
+      }
+    }
 
-    cv::putText(_myCVPtrCopy->image, 
-            "e:"+std::string(toolState_Names[_endoscopeState]),
-            cv::Point(20,50), // Coordinates
-            cv::FONT_HERSHEY_COMPLEX_SMALL, // Font
-            1.0, // Scale. 2.0 = 2x bigger
-            cv::Scalar(255,128,0), // BGR Color
-            1); // Line Thickness (Optional)
-            // cv::CV_AA); // Anti-alias (Optional)
-    
-
-    cv::putText(_myCVPtrCopy->image, 
-            "g:"+std::string(toolState_Names[_grasperState]),
-            cv::Point(420,50), // Coordinates
-            cv::FONT_HERSHEY_COMPLEX_SMALL, // Font
-            1.0, // Scale. 2.0 = 2x bigger
-            cv::Scalar(0,128,255), // BGR Color
-            1); // Line Thickness (Optional)
-            // cv::CV_AA); // Anti-alias (Optional)
-    
-    // Load warning icon
-    cv::Mat baseImage = _myCVPtr->image;
-    cv::Mat warning_icon_resized;
-    cv::Mat warning_icon_to_blend;
-    
-    cv::resize(cv::imread("img/warning.png"),warning_icon_resized,cv::Size(128,128));
-    warning_icon_resized.copyTo(baseImage(cv::Rect(50,50,warning_icon_resized.cols, warning_icon_resized.rows)));
-    
     // Update GUI Window
     cv::imshow(OPENCV_WINDOW, _myCVPtrCopy->image);
     cv::waitKey(3);
 
     // Output modified video stream
     _myOverlayedImagePub.publish(_myCVPtrCopy->toImageMsg());
+}
+void endoscopeModifier::readSurgicalTaskState(const endoscope_feedback::SurgicalTaskStateMsgConstPtr& msg)
+{
+    for (size_t i = 0; i < NB_TOOLS_TYPES; i++)
+    {
+      _toolState[i] = (tool_States) msg->robotMode[i];
+    }
+    
+    if (!_flagSurgicalTaskStateReceived)
+    {
+      _flagSurgicalTaskStateReceived=true;
+    }    
+}
+
+void endoscopeModifier::drawTransparency(cv::Mat frame, cv::Mat transp, int xPos, int yPos) {
+    cv::Mat mask;
+    vector<cv::Mat> layers;
+
+    cv::split(transp, layers); // seperate channels
+    cv::Mat rgb[3] = { layers[0],layers[1],layers[2] };
+    mask = layers[3]; // png's alpha channel used as mask
+    cv::merge(rgb, 3, transp);  // put together the RGB channels, now transp insn't transparent 
+    transp.copyTo(frame.rowRange(yPos, yPos + transp.rows).colRange(xPos, xPos + transp.cols), mask);
+}
+
+
+void endoscopeModifier::loadIcons()
+{
+    // Load warning icon
+    cv::Mat warning_icon_raw = cv::imread(ros::package::getPath("endoscope_feedback")+"/src/img/warning.png",cv::IMREAD_UNCHANGED);
+    cv::Mat warning_icon_to_blend;  
+    Array2i sz_ = (_feedIMGDims * R_WARNING_ICON).cast<int>();
+    cv::resize(warning_icon_raw,_warning_icon_resized,cv::Size(sz_.x(),sz_.y()));    
+}
+
+void endoscopeModifier::putTextForTool(uint toolN_ ,tool_States toolState_, cv::Point point_, cv::Scalar color_)
+{
+  cv::putText(_myCVPtrCopy->image, 
+         std::string(toolID_Names[toolN_])+std::string(toolState_Names[toolState_]),
+         point_, // Coordinates
+         cv::FONT_HERSHEY_COMPLEX_SMALL, // Font
+         1.0, // Scale. 2.0 = 2x bigger
+         color_, // BGR Color
+         1); // Line Thickness (Optional)
+         // cv::CV_AA); // Anti-alias (Optional)
+}
+
+cv::Point2i endoscopeModifier::eigenV2iToCv(Eigen::Vector2i eigenVec2i_)
+{
+  return cv::Point2i(eigenVec2i_.x(),eigenVec2i_.y());
 }
