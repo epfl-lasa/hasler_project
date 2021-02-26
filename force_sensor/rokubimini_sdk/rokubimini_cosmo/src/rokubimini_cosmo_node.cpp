b36eac820001ae2c03612f630afa4d2a06ce3c16
jacob <jacob.hernandezsanchez@epfl.ch>
2021-02-26 16:52:03 +0100

    delete force sensor

--- force_sensor/rokubimini_sdk/rokubimini_cosmo/src/rokubimini_cosmo_node.cpp
+++
@@ -1,8 +0,0 @@
-#include <rokubimini_cosmo/RokubiminiCosmo.hpp>
-
-int main(int argc, char** argv)
-{
-  any_node::Nodewrap<rokubimini_cosmo::RokubiminiCosmo> node(argc, argv, "rokubimini_cosmo", 2, true);
-  node.execute();
-  return 0;
-}
