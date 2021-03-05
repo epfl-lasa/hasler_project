d17ab4af8bba085eb43b415794d103d26b48180f
jacob <jacob.hernandezsanchez@epfl.ch>
2021-03-05 11:05:17 +0100

    remove the force sensor package

--- force_sensor/rokubimini_sdk/any_utils/tinyxml_tools/Readme.md
+++
@@ -1,7 +0,0 @@
-# tinyxml_tools
-
-## Overview
-
-Parse and load paremeters from XML files.
-
-Wraps around the TinyXML library.
--- force_sensor/rokubimini_sdk/rokubimini_ethercat/include/rokubimini_ethercat/soem_interface/common/ThreadSleep.hpp
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-namespace soem_interface
-{
-void threadSleep(const double duration);
-
-}  // namespace soem_interface
